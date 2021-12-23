a8ab19685c9aafc09bfc415649965a66c2869ab7
julianomachads <julianodsm25@gmail.com>
2021-12-23 13:56:24 -0300

    java Servlet: Autenticação, autorização e o padrão MVC.

--- src/br/com/alura/gerenciador/servlet/OiMundoServlet.java
+++ src/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -1,4 +1,4 @@
-package br.com.alura.gerenciador.servlets;
+package br.com.alura.gerenciador.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -8,20 +8,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns="/oi")
-public class OiMundoServlet extends HttpServlet{
-
+//oi
+//@WebServlet(urlPatterns="/oi")
+public class OiMundoServlet extends HttpServlet {
+	
+	public OiMundoServlet() {
+		System.out.println("Criando Oi Mundo Servlet");
+	}
+	
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		
+
 		PrintWriter out = resp.getWriter();
-		
 		out.println("<html>");
 		out.println("<body>");
-		out.println("oi mundo, parabens voce escreveu o primeiro servlets.");
+		out.println("oi mundo, parabens vc escreveu o primeiro servlets.");
 		out.println("</body>");
 		out.println("</html>");
 		
+		System.out.println("o servlet OiMundoServlet foi chamado");
 	}
-	
 }
