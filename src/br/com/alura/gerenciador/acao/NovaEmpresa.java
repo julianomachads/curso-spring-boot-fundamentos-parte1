1442c8525e72006dc686d3af66cc8c14c04031b2
julianomachads <julianodsm25@gmail.com>
2021-12-28 16:50:28 -0300

    Commit referente ao curso spring boot, aula de autenticação, autorização e padrão mvc, começando a atuar com gerenciamento de dependencias e build de aplicações java.

--- src/br/com/alura/gerenciador/acao/NovaEmpresa.java
+++ src/br/com/alura/gerenciador/acao/NovaEmpresa.java
@@ -1,4 +1,4 @@
-package br.com.alura.gerenciador.servlet;
+package br.com.alura.gerenciador.acao;
 
 import java.io.IOException;
 import java.text.ParseException;
@@ -6,23 +6,20 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/alteraEmpresa")
-public class AlteraEmpresaServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+import br.com.alura.gerenciador.modelo.Banco;
+import br.com.alura.gerenciador.modelo.Empresa;
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+public class NovaEmpresa {
 
-		System.out.println("Alterando empresa");
+	public void executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		System.out.println("acao nova empresa");
 		
 		String nomeEmpresa = request.getParameter("nome");
 		String paramDataEmpresa = request.getParameter("data");
-		String paramId = request.getParameter("id");
-		Integer id = Integer.valueOf(paramId);
 		
 		Date dataAbertura = null;
 		try {
@@ -32,15 +29,17 @@ public class AlteraEmpresaServlet extends HttpServlet {
 			throw new ServletException(e);
 		}
 		
-		System.out.println(id);
-		
-		Banco banco = new Banco();
-		Empresa empresa = banco.buscaEmpresaPelaId(id);
+		Empresa empresa = new Empresa();
 		empresa.setNome(nomeEmpresa);
 		empresa.setDataAbertura(dataAbertura);
 		
-		response.sendRedirect("listaEmpresas");
-	
+		Banco banco = new Banco();
+		banco.adiciona(empresa);
+		
+		request.setAttribute("empresa", empresa.getNome());
+		
+		response.sendRedirect("entrada?acao=ListaEmpresas");
+		
 	}
-
+	
 }
